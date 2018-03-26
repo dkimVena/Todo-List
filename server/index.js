ecf5dc870d08572ddf9feeb53418e1d4f4d25939
goginim <david.ingook@gmail.com>
2018-03-25 23:23:47 -0400

    fix deploy

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 const router = express.Router();
 
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../../client/build')));
 
 app.use('/api', api);
 
