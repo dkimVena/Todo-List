ffbf6e5cffc6d72c27bce3045f3609e217181e0c
goginim <david.ingook@gmail.com>
2018-03-25 23:08:16 -0400

    fix deploy

---
+++ server/build/config/dev.js
@@ -0,0 +1,5 @@
+//dev.js don't commit this file
+module.exports = {
+  mongooseURI: 'mongodb://todolist-admin:vndtls2121!@ds139984.mlab.com:39984/todolist',
+  sessionSecret: 'GookMemoLabApp12341$1$234'
+};
---
+++ server/build/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+const keys = require('./config/keys.js');
+const api = require('./src/routes');
+
+const app = express();
+
+// db connection
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => {
+  console.log('Connected to mongodb server');
+});
+mongoose.connect(keys.mongooseURI);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const router = express.Router();
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.use('/api', api);
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+app.use(router);
+app.set('port', process.env.PORT || 3001);
+
+app.listen(app.get('port'), () => {
+  console.log(`Listening on ${app.get('port')}`);
+});
