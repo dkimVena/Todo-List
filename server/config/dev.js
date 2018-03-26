f1b4b9e5b8982bf16778c4e32ac19d89efce11f5
goginim <32228327+goginim@users.noreply.github.com>
2018-03-25 22:45:50 -0400

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,5 +0,0 @@
-//dev.js don't commit this file
-module.exports = {
-  mongooseURI: 'mongodb://todolist-admin:vndtls2121!@ds139984.mlab.com:39984/todolist',
-  sessionSecret: 'GookMemoLabApp12341$1$234'
-};
